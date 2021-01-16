624c790e1bf68a56386f37d2be4f9813aa130445
Administrator <Administrator@DESKTOP-7MLV21C>
2021-01-16 22:11:10 +0300

    initial upload

---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { "Elma","Karpuz"};
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            //type-safe -- tip güvenli
+
+            //backend deniyor bu kısma. arka planda yapılan tasarım işi
+            foreach (var urun in urunler) 
+                // Urun veri tipini ifade eder "var" da yazsak olur
+                // urun takma isimdir.
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+            }
+
+        }
+    }
+}
